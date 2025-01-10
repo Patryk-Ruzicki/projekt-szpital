927a504da39a192c905aa88c093a3132e1090e67
btmnkk <146921148+btmnkk@users.noreply.github.com>
2025-01-10 20:57:02 +0100

    Dodanie komentarzy do Pharmacy.h

--- Pharmacy.h
+++ Pharmacy.h
@@ -1,3 +1,9 @@
+// Plik: Pharmacy.h
+// Autor: Krzysztof Łągiewczyk
+// Data: 10 stycznia 2025
+// Opis: Definicja klasy Pharmacy, która zarządza listą leków dostępnych w aptece
+// oraz umożliwia przypisywanie kosztów leków pacjentom.
+
 #ifndef PHARMACY_H
 #define PHARMACY_H
 
@@ -8,14 +14,21 @@
 
 class Pharmacy {
 private:
+    // Struktura reprezentująca lek z jego nazwą i ceną
     struct Medicine {
-        std::string name;
-        double cost;
+        std::string name; ///< Nazwa leku
+        double cost; ///< Koszt leku
     };
-    std::vector<Medicine> medicines;
+    std::vector<Medicine> medicines; ///< Lista dostępnych leków
+
 public:
+    // Konstruktor klasy Pharmacy. Inicjuje listę dostępnych leków.
     Pharmacy();
+
+    // Przypisuje lek pacjentowi i dodaje koszt leku do całkowitego kosztu leczenia.
     void prescribeMedicine(PatientAccount& patient, int choice);
+
+    // Wyświetla dostępne leki w aptece.
     void showMedicines();
 };
 
