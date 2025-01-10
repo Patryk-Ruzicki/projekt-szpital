688a43ed04f9772a6302a8d0cfb81ec9ea063af3
btmnkk <146921148+btmnkk@users.noreply.github.com>
2025-01-10 20:56:44 +0100

    Dodanie komentarzy do Pharmacy.cpp

--- Pharmacy.cpp
+++ Pharmacy.cpp
@@ -1,24 +1,33 @@
+// Plik: Pharmacy.cpp
+// Autor: Krzysztof Łągiewczyk
+// Data: 4 stycznia 2025
+// Opis: Implementacja klasy Pharmacy. Zawiera metody do zarządzania lekami
+// dostępnymi w aptece oraz przypisywania kosztów pacjentowi.
+
 #include "Pharmacy.h"
 
+// Konstruktor klasy Pharmacy. Inicjuje listę dostępnych leków.
 Pharmacy::Pharmacy() {
-    medicines.push_back({"Lek przeciwbólowy", 50.0});
+    medicines.push_back({"Lek przeciwbolowy", 50.0});
     medicines.push_back({"Antybiotyk", 100.0});
     medicines.push_back({"Antydepresant", 200.0});
     medicines.push_back({"Szczepionka", 150.0});
     medicines.push_back({"Insulina", 300.0});
 }
 
+// Przypisuje lek pacjentowi i dodaje koszt leku do całkowitego kosztu leczenia
 void Pharmacy::prescribeMedicine(PatientAccount& patient, int choice) {
     if (choice >= 1 && choice <= medicines.size()) {
         patient.addCost(medicines[choice - 1].cost);
     } else {
-        std::cout << "Nieprawidłowy wybór leku.\n";
+        std::cout << "Nieprawidlowy wybor leku.\n";
     }
 }
 
+// Wyświetla dostępne leki w aptece
 void Pharmacy::showMedicines() {
-    std::cout << "Dostępne leki:\n";
+    std::cout << "Dostepne leki:\n";
     for (size_t i = 0; i < medicines.size(); ++i) {
-        std::cout << i + 1 << ". " << medicines[i].name << " - " << medicines[i].cost << " zł\n";
+        std::cout << i + 1 << ". " << medicines[i].name << " - " << medicines[i].cost << " PLN\n";
     }
 }
