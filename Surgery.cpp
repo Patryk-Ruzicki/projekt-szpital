a9a1ee1dad26cbd04f80157375151b2daf5fd78c
btmnkk <146921148+btmnkk@users.noreply.github.com>
2025-01-10 20:57:20 +0100

    Dodanie komentarzy do Surgery.cpp

--- Surgery.cpp
+++ Surgery.cpp
@@ -1,13 +1,21 @@
+// Plik: surgery.cpp
+// Autor: Krystian Stróżczyk
+// Data: 4 stycznia 2025
+// Opis: Implementacja klasy Surgery. Zawiera metody do zarządzania operacjami chirurgicznymi
+// oraz przypisywania kosztów operacji pacjentowi.
+
 #include "Surgery.h"
 
+// Konstruktor klasy Surgery. Inicjuje listę dostępnych operacji chirurgicznych.
 Surgery::Surgery() {
-    Operacje.push_back({"Appendectomy", 3500});
-    Operacje.push_back({"Cholecystectomy", 5000});
-    Operacje.push_back({"Knee Arthroscopy", 7500});
-    Operacje.push_back({"Cataract Surgery", 4200});
-    Operacje.push_back({"Heart Bypass Surgery", 20000});
+     Operacje.push_back({"Wyciecie wyrostka", 3500});
+    Operacje.push_back({"Wyciecie pecherzyka zolciowego", 5000});
+    Operacje.push_back({"Artroskopia kolana", 7500});
+    Operacje.push_back({"Operacja zacmy", 4200});
+    Operacje.push_back({"Bypass serca", 20000});
 }
 
+// Wykonuje zabieg chirurgiczny i dodaje koszt zabiegu do całkowitego kosztu leczenia pacjenta
 void Surgery::performSurgery(PatientAccount& patient, int choice) {
     if (choice >= 1 && choice <= Operacje.size()) {
         patient.addCost(Operacje[choice - 1].costOfProcedure);
@@ -16,6 +24,7 @@ void Surgery::performSurgery(PatientAccount& patient, int choice) {
     }
 }
 
+// Wyświetla dostępne operacje chirurgiczne
 void Surgery::displaySurgeries() {
     for (size_t i = 0; i < Operacje.size(); ++i) {
         std::cout << i + 1 << ". " << Operacje[i].surgeryName << " - " << Operacje[i].costOfProcedure << " PLN" << std::endl;
