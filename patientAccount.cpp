a28d2a47d79eb2175cee229db0517886938d3bbd
btmnkk <146921148+btmnkk@users.noreply.github.com>
2025-01-10 20:58:23 +0100

    Dodanie komentarzy do patientAccount.cpp

--- patientAccount.cpp
+++ patientAccount.cpp
@@ -1,19 +1,30 @@
+// Plik: patientAccount.cpp
+// Autor: Patryk Ruzicki
+// Data: 4 stycznia 2025
+// Opis: Implementacja klasy PatientAccount. Zawiera metody do obliczania kosztów
+// leczenia pacjenta oraz zarządzania liczbą dni spędzonych w szpitalu.
+
 #include "patientAccount.h"
 
+// Konstruktor klasy PatientAccount
 PatientAccount::PatientAccount() : totalCost(0), daysInHospital(0) {}
 
+// Dodaje koszt leczenia do całkowitego kosztu pacjenta
 void PatientAccount::addCost(double amount) {
     totalCost += amount;
 }
 
+// Ustawia liczbę dni spędzonych przez pacjenta w szpitalu
 void PatientAccount::setDaysInHospital(int days) {
     daysInHospital = days;
 }
 
+// Oblicza koszt pobytu pacjenta w szpitalu na podstawie liczby dni
 double PatientAccount::calculateHospitalStayCost() const {
     return daysInHospital * dailyRate;
 }
 
+// Zwraca całkowity koszt leczenia pacjenta
 double PatientAccount::getTotalCost() const {
     return totalCost;
 }
