2012984305b10208070ac118fcca49f16ac95be3
btmnkk <146921148+btmnkk@users.noreply.github.com>
2025-01-10 20:57:39 +0100

    Dodanie komentarzy do Surgery.h

--- Surgery.h
+++ Surgery.h
@@ -1,3 +1,9 @@
+// Plik: Surgery.h
+// Autor: Krystian Stróżczyk
+// Data: 4 stycznia 2025
+// Opis: Definicja klasy Surgery, która zarządza operacjami chirurgicznymi
+// i kosztami związanymi z tymi operacjami.
+
 #ifndef SURGERY_H
 #define SURGERY_H
 
@@ -8,14 +14,21 @@
 
 class Surgery {
 private:
+    // Struktura reprezentująca zabieg chirurgiczny z jego nazwą i ceną
     struct Procedure {
-        std::string surgeryName;
-        int costOfProcedure;
+        std::string surgeryName; ///< Nazwa zabiegu chirurgicznego
+        int costOfProcedure; ///< Koszt zabiegu chirurgicznego
     };
-    std::vector<Procedure> Operacje;
+    std::vector<Procedure> Operacje; ///< Lista dostępnych operacji
+
 public:
+    // Konstruktor klasy Surgery. Inicjuje listę dostępnych operacji.
     Surgery();
+
+    // Wykonuje zabieg chirurgiczny i dodaje jego koszt do całkowitego kosztu leczenia pacjenta.
     void performSurgery(PatientAccount& patient, int choice);
+
+    // Wyświetla dostępne operacje chirurgiczne.
     void displaySurgeries();
 };
 
