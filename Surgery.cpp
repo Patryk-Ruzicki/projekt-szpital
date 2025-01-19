cd00f23ff2b38dc45fcb7dc9b93a3008257b08d2
Patryk-Ruzicki <arcysanta@gmail.com>
2025-01-19 23:59:19 +0100

    fixed tab

--- Surgery.cpp
+++ Surgery.cpp
@@ -27,11 +27,14 @@ void Surgery::performSurgery(PatientAccount& patient, int choice) {
 
 // Wyświetla dostępne operacje chirurgiczne
 void Surgery::displaySurgeries() {
+    int colWidth = 35;
+    std::cout << "+----+-----------------------------------+---------------------+\n";
+    std::cout << "| Nr | Nazwa zabiegu             | Koszt zabiegu        |\n";
     std::cout << "+----+-----------------------------------+---------------------+\n";
     for (size_t i = 0; i < Operacje.size(); ++i) {
         std::cout << "| " << std::setw(2) << std::left << i + 1 << " | "
-                << std::setw(colWidth - 2) << std::left << Operacje[i].surgeryName << " | "
-                << std::setw(colWidth - 20) << std::left << Operacje[i].costOfProcedure << " PLN |\n";
+                  << std::setw(colWidth - 2) << std::left << Operacje[i].surgeryName << " | "
+                  << std::setw(colWidth - 20) << std::left << Operacje[i].costOfProcedure << " PLN |\n";
     }
     std::cout << "+----+-----------------------------------+---------------------+\n";
 }
